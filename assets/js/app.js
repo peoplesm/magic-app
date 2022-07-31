9b4fbcc94890bd43bd415ba7d1405d43340ce646
moyuh <mlroth94@gmail..com>
2022-07-31 13:25:17 -0600

    adds semiworking JS for carosel and the API usage

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,40 +1,59 @@
 let mtgURL = "http://api.magicthegathering.io/v1/";
 
-const slides = document.querySelectorAll(".slide");
+const slides = document.querySelectorAll(".slider");
 
 // loop through slides and set each slides translateX property to index * 100% 
-slides.forEach((slide, indx) => {
-  slide.style.transform = `translateX(${indx * 100}%)`;
-});
 
-// current slide counter    
-let curSlide = 0;
+function getImg(){
+  let cardURL = mtgURL + "card";
 
-// select next slide button
-const nextSlide = document.querySelector(".btn-next");
+  fetch(cardURL)
+  .then(function(response){
+    if(response.ok)
+    {response().json().then(function(data){
+      for (let indx = 0; indx < response.length; indx++) {
+        const element = response[i];
+        
+      }
+      slides.forEach((slide, indx) => {
+        slide.style.transform = `translateX(${indx * 100}%)`;
+      });
+      
+      // current slide counter    
+      let curSlide = 0;
+      
+      // select next slide button
+      const nextSlide = document.querySelector(".btn-next");
+      
+      // add event listener and next slide functionality
+      nextSlide.addEventListener("click", function () {
+           curSlide++;
+      
+        slides.forEach((slides, indx) => {
+          slides.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+      });
+      // select prev slide button
+      const prevSlide = document.querySelector(".btn-prev");
+      
+      // add event listener and navigation functionality
+      prevSlide.addEventListener("click", function () {
+        // check if current slide is the first and reset current slide to last
+        if (curSlide === 0) {
+          curSlide = maxSlide;
+        } else {
+          curSlide--;
+        }
+      
+        //   move slide by 100%
+        slides.forEach((slides, indx) => {
+          slides.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+        });
+      });
+      }
+    )}
+}
+)
+}   
 
-// add event listener and next slide functionality
-nextSlide.addEventListener("click", function () {
-     curSlide++;
 
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
-// select prev slide button
-const prevSlide = document.querySelector(".btn-prev");
-
-// add event listener and navigation functionality
-prevSlide.addEventListener("click", function () {
-  // check if current slide is the first and reset current slide to last
-  if (curSlide === 0) {
-    curSlide = maxSlide;
-  } else {
-    curSlide--;
-  }
-
-  //   move slide by 100%
-  slides.forEach((slide, indx) => {
-    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
-  });
-});
