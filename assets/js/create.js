573973842dbda4144f81e145f30acdc0819f9f9f
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-08-03 17:09:08 -0600

    dbl clicking recently added li deletes the element and the object in the storing array

--- assets/js/create.js
+++ assets/js/create.js
@@ -2,6 +2,7 @@ let searchInputEl = document.querySelector(".searchInput");
 let searchBtn = document.querySelector(".search");
 let resultsEl = document.querySelector(".search-results");
 let recentAddEl = document.querySelector(".recently-added");
+let saveListBtn = document.querySelector(".save-list");
 
 function handleInput(event) {
   event.preventDefault();
@@ -12,34 +13,72 @@ function handleInput(event) {
   searchInputEl.value = "";
 }
 
-searchBtn.addEventListener("click", handleInput);
-
 function searchCard(searchInput) {
   let apiUrl = `https://api.scryfall.com/cards/search?q=${searchInput}`;
   fetch(apiUrl).then(function (response) {
     if (response.ok) {
-      response.json().then(function (data) {
-        console.log(data);
-        for (let i = 0; i < data.data.length; i++) {
-          let cardImg = document.createElement("img");
-          resultsEl.append(cardImg);
-          cardImg.setAttribute("src", data.data[i].image_uris.normal);
-          cardImg.setAttribute("style", "width: 150px; border-radius: 10px");
-          cardImg.setAttribute("data-name", data.data[i].name);
-          cardImg.classList.add("card-result-img");
-          cardImg.addEventListener("click", handleCardClick);
-        }
-      });
+      response
+        .json()
+        .then(function (data) {
+          console.log(data);
+          for (let i = 0; i < data.data.length; i++) {
+            let cardImg = document.createElement("img");
+            resultsEl.append(cardImg);
+            cardImg.setAttribute("src", data.data[i].image_uris.normal);
+            cardImg.setAttribute(
+              "style",
+              "width: 150px; border-radius: 10px; z-index: 5"
+            );
+            cardImg.setAttribute("data-name", data.data[i].name);
+            cardImg.classList.add("card-result-img");
+            cardImg.addEventListener("click", handleCardClick);
+          }
+        })
+        .catch((error) => {
+          console.log("error", error);
+        });
     }
   });
 }
 
+let savedCardArr = [];
 function handleCardClick(event) {
-  console.log(event.target.attributes["data-name"].nodeValue);
+  console.log(event);
   let cardName = event.target.attributes["data-name"].nodeValue;
+  let cardImg = event.target.currentSrc;
+  let cardObj = {
+    cardName: cardName,
+    cardImg: cardImg,
+  };
+  savedCardArr.push(cardObj);
   let cardLi = document.createElement("li");
   recentAddEl.append(cardLi);
   cardLi.textContent = cardName;
   cardLi.classList.add("list-group-item");
   cardLi.classList.add("card-li");
+  cardLi.classList.add("user-select-none");
+  cardLi.setAttribute("data-mdb-toggle", "tooltip");
+  cardLi.setAttribute("title", "Double Click to Remove");
+  cardLi.addEventListener("dblclick", removeLi);
+  console.log(savedCardArr);
 }
+
+function removeLi(event) {
+  console.log(event);
+  let cardLi = event.target;
+  let targetCardName = event.target.innerText;
+  console.log(targetCardName);
+
+  let cardIndex = savedCardArr
+    .map((cardName) => cardName.cardName)
+    .indexOf(`${targetCardName}`);
+  console.log(cardIndex);
+  savedCardArr.splice(cardIndex, 1);
+  cardLi.remove();
+  console.log(savedCardArr);
+}
+
+function handleSave() {}
+
+searchBtn.addEventListener("click", handleInput);
+// saveListBtn.addEventListener("click", handleSave);
