ecd0a1567f91c90847851daa5237bfe3a4e8fd36
peoplesm <105310316+peoplesm@users.noreply.github.com>
2022-08-04 21:33:17 -0600

    deck and input data are saving to local storage, wont refresh correctly on wishlist page

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,6 +1,6 @@
 let arrCardWishlist = [];
 let cardWishlistEl = document.getElementById("cards-id");
-let formArr =[];
+let formArr = [];
 
 let gifyURL =
   "https://api.giphy.com/v1/gifs/search?api_key=nP9BkrRS5LsHhVROKIrah4GzQ3R1k7PV&q=";
@@ -17,67 +17,61 @@ let gifSection = document.querySelector(".gif-section");
 let comment = document.createElement("p");
 let listName = document.createElement("p");
 
-function getGIF() {
+function getGIF(deckObject) {
   let gifyAPI =
     gifyURL + gifSearch.value.trim() + "&limit=1&offset=0&rating=pg&lang=en";
   fetch(gifyAPI).then(function (response) {
-    fetch(gifyAPI);
     if (response.ok) {
       response.json().then(function (data) {
         console.log(response);
         let gifAttach = data.data[0].images.original.url;
+        deckObject.formGif = gifAttach;
+        arrCardWishlist.unshift(deckObject);
+
+        localStorage.setItem("deck", JSON.stringify(arrCardWishlist));
+
         // let addGif = document.innerHTML= '<img src =  "'+ gifAttach +'" title="GIF">';
         let addGif = document.createElement("img");
         addGif.setAttribute("src", gifAttach);
         addGif.classList.add("gifClass");
         gifSection.append(addGif);
+        return gifAttach;
       });
     }
-  });
+  }); //add a catch or an empty string formGif
 }
 
 function formSubmission() {
-  deckObject= {
+  deck = {
     formName: wishName.value.trim(),
     formDate: wishDate.value.trim(),
     formComment: wishComment.value,
-    formGif: getGIF(),
-    cards: displayCardz()
+    // formGif: getGIF(),
   };
-  // formInfo.textContent = wishName.value.trim();
-  // date.textContent = wishDate.value.trim();
-  // comment.textContent = wishComment.value;
-
-  // formInfo.append(date);
-  // date.append(comment);
-  localStorage.setItem("form-info", JSON.stringify(deckObject));
-
-  JSON.parse(localStorage.getItem("form-info"));
-  listName.textContent = deckObject.formName;
-  comment.textContent = deckObject.formComment;
-  date.textContent = deckObject.formDate;
+  getGIF(deck);
+  JSON.parse(localStorage.getItem("deck"));
+  listName.textContent = deck.formName;
+  comment.textContent = deck.formComment;
+  date.textContent = deck.formDate;
 
   formInfo.append(listName);
   formInfo.append(date);
-  formInfo.append(comment)
-  
-
+  formInfo.append(comment);
 
   return;
 }
 
 //event listeners:
 // gif.addEventListener("click", function () {
- 
+
 // });
 
 // displaying cards on wishlist
 function displayCardz() {
-  arrCardWishlist = JSON.parse(localStorage.getItem("deck"));
   console.log(arrCardWishlist);
   for (let index = 0; index < arrCardWishlist.length; index++) {
-    var cardImage = arrCardWishlist[index].cardImg;
-    var cardImageEl = document.createElement("img");
+    let cardImage = arrCardWishlist[index].cardImg;
+    let cardImageEl = document.createElement("img");
     cardImageEl.setAttribute("src", cardImage);
     cardWishlistEl.append(cardImageEl);
   }
@@ -86,8 +80,14 @@ function displayCardz() {
 submitBtn.addEventListener("click", function (event) {
   event.preventDefault();
   formSubmission();
-  // displayCardz();
   // getGIF();
 });
 
-
+document.onreadystatechange = function () {
+  document.onreadystatechange = function () {
+    if (localStorage.getItem("deck")) {
+      arrCardWishlist = JSON.parse(localStorage.getItem("deck"));
+      displayCardz();
+    }
+  };
+};
